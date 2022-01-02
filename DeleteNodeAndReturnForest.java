b3854a2966d1323ad344b8fa4b289ad7efd1ef72
Karteek <mkarteek27@gmail.com>
2022-01-02 11:10:53 +0530

    Add files via upload

--- DeleteNodeAndReturnForest.java
+++ DeleteNodeAndReturnForest.java
@@ -1,44 +1,93 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 public class DeleteNodeAndReturnForest {
 
-	List<TreeNode> forest = null;
-
 	public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
-		
-		List<TreeNode> forest = new ArrayList<>();
-		if (root == null)
-			return forest;
-		Set<Integer> set = new HashSet<>();
-		for (int i : to_delete) {
-			set.add(i);
+
+		Set<Integer> integersTobeDeletedFromTree = new HashSet<Integer>();
+		for (int number : to_delete) {
+			integersTobeDeletedFromTree.add(number);
 		}
-		deleteNodes(root, set, forest);
-		if (!set.contains(root.val)) {
-			forest.add(root);
+		List<TreeNode> result = new ArrayList<TreeNode>();
+
+		delNodes(root, integersTobeDeletedFromTree, result);
+		if (!integersTobeDeletedFromTree.contains(root.val)) {
+			result.add(root);
 		}
-		return forest;
+
+		return result;
 	}
 
-	private TreeNode deleteNodes(TreeNode node, Set<Integer> set, List<TreeNode> forest) {
-		if (node == null)
+	private TreeNode delNodes(TreeNode node, Set<Integer> integersTobeDeletedFromTree, List<TreeNode> result) {
+
+		if (null == node) {
 			return null;
+		}
 
-		node.left = deleteNodes(node.left, set, forest);
-		node.right = deleteNodes(node.right, set, forest);
+		node.left = delNodes(node.left, integersTobeDeletedFromTree, result);
+		node.right = delNodes(node.right, integersTobeDeletedFromTree, result);
 
-		if (set.contains(node.val)) {
-			if (node.left != null)
-				forest.add(node.left);
-			if (node.right != null)
-				forest.add(node.right);
+		if (integersTobeDeletedFromTree.contains(node.val)) {
+			if (node.left != null) {
+				result.add(node.left);
+			}
+			if (node.right != null) {
+				result.add(node.right);
+			}
+
 			return null;
 		}
 
 		return node;
+
 	}
 
+	public List<TreeNode> delNodes2(TreeNode root, int[] to_delete) {
+
+		Set<Integer> integersTobeDeletedFromTree = new HashSet<Integer>();
+		for (int number : to_delete) {
+			integersTobeDeletedFromTree.add(number);
+		}
+
+		List<TreeNode> result = new ArrayList<TreeNode>();
+		if (null == root) {
+			return result;
+		}
+
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root);
+		result.add(root);
+		while (!queue.isEmpty()) {
+			TreeNode currentNode = queue.remove();
+			if (integersTobeDeletedFromTree.contains(currentNode.val)) {
+				result.remove(currentNode);
+				if (currentNode.left != null) {
+					result.add(currentNode.left);
+				}
+				if (currentNode.right != null) {
+					result.add(currentNode.right);
+				}
+			}
+
+			if (currentNode.left != null) {
+				queue.add(currentNode.left);
+				if (integersTobeDeletedFromTree.contains(currentNode.left.val)) {
+					currentNode.left = null;
+				}
+			}
+
+			if (currentNode.right != null) {
+				queue.add(currentNode.right);
+				if (integersTobeDeletedFromTree.contains(currentNode.right.val)) {
+					currentNode.right = null;
+				}
+			}
+		}
+		return result;
+	}
 }
