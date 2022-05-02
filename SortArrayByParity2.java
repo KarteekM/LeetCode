0055161f17164edc04ac313f4d4e341336022608
Karteek <mkarteek27@gmail.com>
2022-05-02 07:01:38 +0530

    Add files via upload

---
+++ SortArrayByParity2.java
@@ -0,0 +1,24 @@
+
+public class SortArrayByParity2 {
+
+	public int[] sortArrayByParity(int[] nums) {
+
+		int i = 0;
+		int j = nums.length - 1;
+
+		while (i < j) {
+
+			if (nums[i] % 2 == 0) {
+				i++;
+			} else {
+				int temp = nums[i];
+				nums[i] = nums[j];
+				nums[j--] = temp;
+			}
+
+		}
+
+		return nums;
+	}
+
+}
