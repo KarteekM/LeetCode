485557086141a6615af0835f85447c223fc9afca
Karteek <mkarteek27@gmail.com>
2022-06-03 19:20:41 +0530

    Update MergeTwoSortedLists.java

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -23,34 +23,87 @@ public class MergeTwoSortedLists {
 		ListNode head = new ListNode(-1);
 		ListNode traverser = head;
 		while (l1 != null && l2 != null) {
-			
-			if(l1.val <= l2.val ) {
+
+			if (l1.val <= l2.val) {
 				traverser.next = l1;
 				l1 = l1.next;
-			}
-			else {
+			} else {
 				traverser.next = l2;
 				l2 = l2.next;
 			}
-			
+
 			traverser = traverser.next;
-			
+
 		}
-		
-		if( l1 != null) {
-			
+
+		if (l1 != null) {
+
 			traverser.next = l1;
-		}
-		else if( l2 != null) {
+		} else if (l2 != null) {
 			traverser.next = l2;
 		}
 
 		return head;
 
 	}
-	
+
+	ListNode traverser;
+
+	public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+		if (l1 == null && l2 == null) {
+			return null;
+		}
+		ListNode head = new ListNode(-1);
+		traverser = head;
+		helper(l1, l2);
+		return head.next;
+
+	}
+
+	private void helper( ListNode l1, ListNode l2) {
+		if (l1 != null && l2 != null) {
+
+			if (l1.val <= l2.val) {
+				traverser.next = l1;
+				l1 = l1.next;
+			} else {
+				traverser.next = l2;
+				l2 = l2.next;
+			}
+			traverser = traverser.next;
+
+		}
+
+		if (l1 == null && l2 == null) {
+			return;
+		}
+
+		else if (l1 != null && l2 == null) {
+			traverser.next = l1;
+			return;
+		}
+
+		else if (l1 == null && l2 != null) {
+			traverser.next = l2;
+			return;
+		}
+
+		helper(l1, l2);
+	}
+
 	public static void main(String[] args) {
-		
+
+		ListNode l1 = new ListNode(1);
+		l1.next = new ListNode(3);
+		l1.next.next = new ListNode(5);
+		l1.next.next.next = new ListNode(7);
+
+		ListNode l2 = new ListNode(2);
+		l2.next = new ListNode(4);
+		l2.next.next = new ListNode(6);
+		l2.next.next.next = new ListNode(8);
+
+		ListNode result = new MergeTwoSortedLists().mergeTwoLists2(l1, l2);
 	}
 
 }
