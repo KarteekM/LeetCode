c5389a97b3394fc7c71edda147bc5a0ff2c8bbc7
Karteek <mkarteek27@gmail.com>
2023-04-18 18:21:26 +0100

    Add files via upload

--- ValidateBinarySearchTree.java
+++ ValidateBinarySearchTree.java
@@ -1,26 +1,75 @@
+package blind75.trees;
+
+import com.sun.source.tree.*;
 
 public class ValidateBinarySearchTree {
 
-	public boolean isValidBST(TreeNode root) {
+    //Without taking help of MIN and MAX
+    public boolean isValidBST(TreeNode root) {
 
-		if (null == root) {
-			return false;
-		}
-		return helperForCheckingBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        if(null == root){
+            return false;
+        }
 
-	}
+        return isBinaryTree(root,null,null);
 
-	private boolean helperForCheckingBST(TreeNode root, long minValue, long maxValue) {
-		if (null == root) {
-			return true;
-		}
+    }
 
-		if (root.val <= minValue || root.val >= maxValue)
-			return false;
 
-		return true && helperForCheckingBST(root.left, minValue, root.val)
-				&& helperForCheckingBST(root.right, root.val, maxValue);
+    private boolean isBinaryTree(TreeNode root, TreeNode left, TreeNode right) {
 
-	}
+        if(null == root){
+            return true;
+        }
+
+        if(left != null && root.val <= left.val){
+            return  false;
+        }
+
+        if(right != null && root.val >= right.val){
+            return  false;
+        }
+
+        return isBinaryTree(root.left, left, root) && isBinaryTree(root.right, root, right);
+    }
+
+    //Taking help of MIN and MAX
+    public boolean isValidBST2(TreeNode root) {
+
+        if(null == root){
+            return false;
+        }
+
+        return isBinaryTree2(root, Integer.MAX_VALUE,Integer.MIN_VALUE);
+
+    }
+
+    private boolean isBinaryTree2(TreeNode root, int left, int right) {
+        if (root == null) return true;
+        if(root.val <= left || root.val >=  right){
+            return false;
+        }
+        return isBinaryTree2(root.left,left,root.val) && isBinaryTree2(root.right,root.val,right);
+    }
+
+    //InOrder
+    private TreeNode prev;
+    public boolean isValidBST3(TreeNode root) {
+        prev = null;
+        return inOrder(root);
+    }
+
+    private boolean inOrder(TreeNode root) {
+        if (root == null) return true;
+
+        if (!inOrder(root.left)) return false;
+
+        // Operate on root
+        if (prev != null && prev.val >= root.val) return false;
+        prev = root;
+
+        return inOrder(root.right);
+    }
+
 
 }
